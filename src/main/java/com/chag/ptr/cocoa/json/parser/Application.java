5e74281ac8df5229bac69546d64cf9ded05e4783
shumpei.haga <shumpei.haga@rakuten.com>
2022-07-23 17:27:52 +0900

    initial commit

---
+++ src/main/java/com/chag/ptr/cocoa/json/parser/Application.java
@@ -0,0 +1,13 @@
+package com.chag.ptr.cocoa.json.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/chag/ptr/cocoa/json/parser/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chag.ptr.cocoa.json.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
