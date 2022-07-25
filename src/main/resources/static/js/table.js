546dadc22b49d71e4aee992bf4f5633f487ce0b7
shumpei.haga <shumpei.haga@rakuten.com>
2022-07-25 23:59:09 +0900

    implement css

--- src/main/resources/static/js/table.js
+++ src/main/resources/static/js/table.js
@@ -4,22 +4,20 @@ if (result.isError) {
     document.getElementById("resultTable").innerText = "JSONの解析に失敗しました。フォーマットが不正な可能性があります。";
 
 } else if (result.length > 0) {
-    let tableHtml = `<table border="1"><thead><tr><th>日付</th><th>接触回数</th><th>リスクスコア</th><th>接触時間</th><th>最小db</th><th>最大db</th></tr></thead>`
+    let insertHtml = "";
+    let i = 0;
     result.forEach(elm => {
-        const size = elm.cl.length;
-        let isFirst = true;
+        insertHtml += `<div class="collapsable-toggle"><a data-bs-toggle="collapse" data-bs-target="#clps${i}" aria-expanded="false" aria-controls="clps${i}">
+            ${elm.d} 接触数: ${elm.cn} リスクスコア: ${elm.ss}</a></div>`;
+        insertHtml += `<div class="collapse" id="clps${i++}"><table><thead><th>No</th><th>接触時間(分)</th><th>最小db</th><th>最大db</th></tr></thead><tbody>`;
+        let idx = 1;
         elm.cl.forEach(info => {
-            if (isFirst) {
-                tableHtml += `<tbody><tr><td rowspan="${size}">${elm.d}</td><td rowspan="${size}">${elm.cn}</td><td rowspan="${size}">${elm.ss}</td>`
-                isFirst = false;
-            } else {
-                tableHtml += `<tr>`
-            }
-            tableHtml += `<td>${info.cm}</td><td>${info.miad}</td><td>${info.maad}</td></tr>`
+            insertHtml += `<tr><td>${idx++}</td><td>${info.cm}</td><td>${info.miad}</td><td>${info.maad}</td></tr>`;
         });
+        insertHtml += `</tbody></table></div>`;
     });
-    tableHtml += `</tbody></table>`
+
     const table = document.createElement("div");
-    table.insertAdjacentHTML('afterbegin', tableHtml)
+    table.insertAdjacentHTML('afterbegin', insertHtml);
     document.getElementById("resultTable").replaceWith(table);
 }
